be5f1be184644a8211b42be52c2e0952829ee1f6
Luan Dias <lumardias07@gmail.com>
2025-08-06 13:39:42 -0300

    Update Empregado.java

--- CursoNovo/heranca-e-polimorfismo/src/Empregado.java
+++ CursoNovo/heranca-e-polimorfismo/src/Empregado.java
@@ -3,12 +3,9 @@ public sealed abstract class Empregado permits Gerente, Vendedor {
     private String codigo;
     private String nome;
     private String endereco;
-<<<<<<< HEAD
     private int age;
     private double salario;
-=======
     private int salario;
->>>>>>> versaoDeteste
 
 
     public String getCodigo() {
@@ -17,27 +14,18 @@ public sealed abstract class Empregado permits Gerente, Vendedor {
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> versaoDeteste
     public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> versaoDeteste
     public String getEndereco() {
         return endereco;
     }
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
-<<<<<<< HEAD
     public int getAge() {
         return age;
     }
@@ -50,13 +38,11 @@ public sealed abstract class Empregado permits Gerente, Vendedor {
     }
 
     public void setSalario(double salario) {
-=======
 
     public int getSalario() {
         return salario;
     }
     public void setSalario(int salario) {
->>>>>>> versaoDeteste
         this.salario = salario;
     }
     
